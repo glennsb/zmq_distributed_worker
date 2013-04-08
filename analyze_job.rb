1f291599e4a4b67d96446ec6ba276c68c395aef2
Stuart Glenn <Stuart-Glenn@omrf.org>
2013-04-08 11:15:55 -0500

    Adds explicity log file close if it is is there. Also random delay flag to the analyze

--- analyze_job.rb
+++ analyze_job.rb
@@ -15,15 +15,19 @@ class AnalyzeJob
       log_file.sync = true
       logger = OMRF::FstreamLogger.new(log_file,log_file)
       conf_file = File.join(@base_dir,"..","..","metadata","analysis_config.yml")
-      cmd = "analyze_sequence_to_snps.rb -d 0 --local -o #{@base_dir} -c  #{conf_file} #{@sample_id}"
+      cmd = "analyze_sequence_to_snps.rb -d 120 --local -o #{@base_dir} -c  #{conf_file} #{@sample_id}"
       c = OMRF::LoggedExternalCommand.new(cmd,logger)
       if c.run
+        log_file.close() if log_file
         return {:exit_status => 0}
       else
+        log_file.close() if log_file
         return {:exit_status => c.exit_status}
       end
+      log_file.close() if log_file
       return {:exit_status => -1}
     rescue => err
+      log_file.close() if log_file
       return {:exit_status => -1, :trace=>err.backtrace}
     end
   end
